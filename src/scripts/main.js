5c79ea8ef42383b10946569ec8fd75972f6eab7e
haduigon <haduigon@gmail.com>
2024-04-25 21:53:36 +0300

    seems, have finished top part but it is not for sure

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,15 +1,2 @@
 'use strict';
 
-// console.log(cactusWidth);
-// function setBlueSlogan() {
-//   const screenWidth = window.innerWidth;
-//   console.log(screenWidth);
-//   // if (screenWidth < 768) {
-//   //   const cactusWidth = document.getElementById('cactus').offsetHeight;
-//   //   const blueText = document.getElementById('blue-text');
-//   //   blueText.style.transform = `translateY(${cactusWidth - 100}px)`;
-//   // }
- 
-// }
-// window.addEventListener('load', setBlueSlogan);
-// window.addEventListener('resize', setBlueSlogan);
